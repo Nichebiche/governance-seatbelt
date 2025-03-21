1528f10de5c26d06e572a500e416d059e0de0beb
marcomariscal <42938673+marcomariscal@users.noreply.github.com>
2025-03-21 12:10:56 -0700

    feat: addr

--- utils/contracts/erc20.ts
+++ utils/contracts/erc20.ts
@@ -1,9 +1,9 @@
-import { erc20Abi, erc20Abi_bytes32, getAddress } from 'viem';
+import { erc20Abi, erc20Abi_bytes32, getAddress, type Address } from 'viem';
 import { publicClient } from '../clients/client';
 
 const SAI = '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359';
 
-export async function fetchTokenMetadata(address: string) {
+export async function fetchTokenMetadata(address: Address) {
   const tokenAddress = getAddress(address);
   const abi = tokenAddress === SAI ? erc20Abi_bytes32 : erc20Abi;
   const contract = { abi, address: tokenAddress } as const;
