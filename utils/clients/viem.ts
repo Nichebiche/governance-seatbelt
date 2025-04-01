d2e4d32ace165fc0aa20ecc15cbf619660a371d9
marcomariscal <42938673+marcomariscal@users.noreply.github.com>
2025-04-01 15:32:19 -0700

    fix: provide rpc url

--- utils/clients/viem.ts
+++ utils/clients/viem.ts
@@ -1,7 +1,11 @@
 import { http, createPublicClient } from 'viem';
 import { mainnet } from 'viem/chains';
 
+if (!process.env.RPC_URL) {
+  throw new Error('RPC_URL is not set');
+}
+
 export const publicClient = createPublicClient({
   chain: mainnet,
-  transport: http(),
+  transport: http(process.env.RPC_URL),
 });
